30c95ac3d9574fb81b6dc10646a883080e7ffefa
wbargerb <wim.bargerbos@capgemini.com>
2022-06-10 10:58:28 +0200

    update since long time

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,39 @@
+class CategoriesController < ApplicationController
+
+  before_action :require_admin, except: [:index, :show]
+
+  def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
+  end
+
+  def create 
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Succesfully created cato"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+  end
+
+end
